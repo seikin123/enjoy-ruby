787a2aa6faf65e7b36a5605017227f9358688515
ヨシダタイキ <abc521fjr7@gmail.com>
2021-03-13 21:34:00 +0900

    電子メールのヘッダを解析する処理について

---
+++ example_case.rb
@@ -0,0 +1,15 @@
+text.each_line do |line|
+
+  case line
+  when /^From:/i
+    puts "送信者の情報を見つけました"
+  when /^To:i
+    puts "宛先の情報を見つけました"
+  when /^$/
+    #$は、行の先頭の直後に行末が来る場合、すなわち行の中身が空の場合に真になる正規表現
+    #つまり空行のこと
+    puts "ヘッダの解析が終了いたしました"
+  else
+    # 読み飛ばす
+  end
+end
