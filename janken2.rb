66f6f8461cbac07a22ae2bbb9ee35b8fed0874ba
ヨシダタイキ <abc521fjr7@gmail.com>
2021-03-15 00:42:05 +0900

    ジャンケンプログラムの復習

--- janken2.rb
+++ janken2.rb
@@ -5,7 +5,7 @@ hands = ["グー", "チョキ", "パー"] #配列を使用
 next_game = true  #while文を使う
 
 while next_game do
-  #常に表示させている部分
+  #最初に表示させている部分
   puts "最初はグージャンケン..."
   puts "[0]:グー"
   puts "[1]:チョキ"
@@ -13,8 +13,24 @@ while next_game do
   player_number = gets.to_i
   enemy_number = rand(3) #乱数
 
-  if player_number >= 3
+  if player_number >= 3 #3以上が入力された場合は無効と表示しループさせる
     puts "入力された値は無効です"
-  elseif player_number == enemy_number
+  elsif player_number == enemy_number
+    puts "あなたの手#{hands[player_number]}"
+    puts "相手の手#{hands[enemy_hands]}"
     puts "あいこです"
+    break
+  elsif (player_number == 0 && enemy_number == 1) || (player_number == 1 && enemy_number == 2)||
+          (player_number == 2 && enemy_number == 0)
+          puts "あなたの手#{hands[player_number]}"
+          puts "相手の手#{hands[enemy_number]}"
+          puts "あなたの勝ちです"
+          break
+  else
+    puts "あなたの手#{hands[player_number]}"
+    puts "相手の手#{hands[enemy_number]}"
+    puts "あなたの負けです"
+    break
+  end
+end
 
