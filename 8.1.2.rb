4a60c06fb6381045a288faa2e231264801621526
ヨシダタイキ <abc521fjr7@gmail.com>
2021-03-31 05:51:24 +0900

    instance_ofメソッドの追加

--- 8.1.2.rb
+++ 8.1.2.rb
@@ -2,4 +2,14 @@ ary = Array.new
 p ary.class
 p "ABC".class  
 
+#instance_ofメソッド-クラスのインスタンスであることを調べる
+ary = Array.new
+str = "Hello!"
+p ary.instance_of?(Array)
+p str.instance_of?(String)
+p ary.instance_of?(String)
 
+#is_a?継承関係を遡ってクラスに属するか調べる
+str = "This is a String."
+p str.is_a?(String)
+p str.is_a?(Object)
